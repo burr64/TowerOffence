03305eb4b9590a1d94ff107d6a9bb95b88bc0a7e
burr64 <zoory@list.ru>
2025-12-11 18:41:09 +0400

    mb last test

---
+++ Assets/Scripts/GridManager.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GridManager : MonoBehaviour
+{
+    [SerializeField] private int _width, _height;
+    [SerializeField] private Tile _tilePrefab;
+    [SerializeField] private Transform _cam;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        GenerateGrid();   
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void GenerateGrid(){
+        for (int x = 0; x < _width; x++){
+            for (int y = 0; y < _height; y++){
+                var spawnedTile = Instantiate(_tilePrefab, new Vector3(x,y), Quaternion.identity);
+                spawnedTile.name = $"Tile {x} {y}";
+            }
+        }
+
+        //_cam.transform.position
+    }
+}
