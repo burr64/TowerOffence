bbf5771874ca421ed8ac49d036abe0d4e3b727fc
burr64 <zoory@list.ru>
2025-12-11 17:28:59 +0400

    test script

---
+++ Assets/Scripts/KreepController.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KreepController : MonoBehaviour
+{
+    [SerializeField] private int speed;
+    [SerializeField] private int damage_per_second;
+    [SerializeField] private int hp;
+    [SerializeField] private int price;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
