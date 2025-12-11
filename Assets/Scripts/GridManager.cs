504a02d006b8e4f6e11711219a71325707a82526
burr64 <zoory@list.ru>
2025-12-11 18:51:18 +0400

    gridManager 1

--- Assets/Scripts/GridManager.cs
+++ Assets/Scripts/GridManager.cs
@@ -26,5 +26,10 @@ public class GridManager : MonoBehaviour
                 spawnedTile.name = $"Tile {x} {y}";
             }
         }
+<<<<<<< Updated upstream
+=======
+
+        _cam.transform.position = new Vector3((float)_width/2 - 0.5f, (float)_height/2 -  0.5f); 
+>>>>>>> Stashed changes
     }
 }
