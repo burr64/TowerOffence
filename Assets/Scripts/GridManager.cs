2cfd30857f77a9f97a6466fda89f9ed865418db0
burr64 <zoory@list.ru>
2025-12-13 21:28:27 +0400

    map1

--- Assets/Scripts/GridManager.cs
+++ Assets/Scripts/GridManager.cs
@@ -1,32 +1,45 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
-public class GridManager : MonoBehaviour
-{
-    [SerializeField] private int _height, _width;
+ 
+public class GridManager : MonoBehaviour {
+    [SerializeField] private int _width, _height;
+ 
     [SerializeField] private Tile _tilePrefab;
+ 
     [SerializeField] private Transform _cam;
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        GenerateGrid();   
+ 
+    private Dictionary<Vector2, Tile> _tiles;
+    private float _tileSize;
+ 
+    void Start() {
+        GenerateGrid();
     }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
-    void GenerateGrid(){
-        for (int x = 0; x < _width; x++){
-            for (int y = 0; y < _height; y++){
-                var spawnedTile = Instantiate(_tilePrefab, new Vector3(x,y), Quaternion.identity);
+ 
+    void GenerateGrid() {
+        _tiles = new Dictionary<Vector2, Tile>();
+        _tileSize = _tilePrefab.GetComponent<SpriteRenderer>().bounds.size.x;
+        for (int x = 0; x < _width; x++) {
+            for (int y = 0; y < _height; y++) {
+                var spawnedTile = Instantiate(_tilePrefab, new Vector3(x * _tileSize, y * _tileSize), Quaternion.identity);
                 spawnedTile.name = $"Tile {x} {y}";
+ 
+                bool randomOffset = UnityEngine.Random.value > 0.5f;
+                spawnedTile.Init(randomOffset);
+                _tiles[new Vector2(x, y)] = spawnedTile;
             }
         }
-        _cam.transform.position = new Vector3((float)_width/2 - 0.5f, (float)_height/2 -  0.5f); 
+ 
+        _cam.position = new Vector3(
+            (_width * _tileSize) / 2f - _tileSize / 2f,
+            (_height * _tileSize) / 2f - _tileSize / 2f,
+            -10
+        );
+    }
+ 
+    public Tile GetTileAtPosition(Vector2 pos) {
+        if (_tiles.TryGetValue(pos, out var tile)) return tile;
+        return null;
     }
 }
